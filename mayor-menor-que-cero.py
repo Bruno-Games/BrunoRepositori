1e00b64a737e3854c4eeb5f30e390f04fa34245b
Bruno-Games <54452777+Bruno-Games@users.noreply.github.com>
2019-08-30 17:03:18 -0300

    Create mayor-menor-que-cero.py
    version final

---
+++ mayor-menor-que-cero.py
@@ -0,0 +1,13 @@
+print ("Ingrese un mumero: ")
+num = int(input())
+
+if num < 0 :
+
+    print ("El numero es menor que cero")
+
+else:
+
+    print ("El numero es mayor que cero")
+
+print("Presione una tecla para continuar..")
+esperar = input()
